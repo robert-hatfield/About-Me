bbf2f958ef32305b396f3a66cdf220a36a6fdc2c
robert-hatfield <robert@lucidthought.xyz>
2017-01-03 14:44:54 -0800

    Inital JavaScript working

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var answer1 = confirm('Are you ready?');
+if (answer1 === true) {
+  console.log('User has confirmed readiness.');
+} else {
+  console.log('User is NOT prepared.');
+}
