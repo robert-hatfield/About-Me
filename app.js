1ecc96568bdc5ff012edee82cbf1d64c4a90ca68
robert-hatfield <robert@lucidthought.xyz>
2017-01-03 15:54:51 -0800

    Checks for yes or no responses. Need to fix null responses; toUpperCase is undefined.

--- app.js
+++ app.js
@@ -6,23 +6,51 @@ var answer1 = confirm('You\'re about to to make some guesses about Rob Hatfield.
 // Log user response for readiness. No quiz if user clicks 'Cancel'.
 if (answer1 === true) {
   console.log('User has confirmed readiness.');
+  alert('There are 5 Yes or No questions, so please answer accordingly.');
 
-  var sleepGuess = prompt('Question 1: Do you think Rob has had more than six hours of sleep in the past two days?');
+  var sleepGuess = prompt('Question 1: Do you think Rob has had more than six hours of sleep in the past two days? \(Y\/N\)');
   // Because he sure hasn't.
-  console.log('User guessed: ' + sleepGuess);
-  var motorcycleGuess = prompt('Question 2: Do you think Rob rides a motorcycle?');
-  // I was *this* close!
-  console.log('User guessed: ' + motorcycleGuess);
-  var montyPythonGuess = prompt('Question 3: Do you agree that three shall be the number thou shalt count, and the number of the counting shall be three?');
-  // Five is right out.
-  console.log('User guessed: ' + montyPythonGuess);
-  var anglophileGuess = prompt('Question 4: Do you think Rob also likes Red Dwarf and Doctor Who?');
-  // Warning: possible Anglophile detected.
-  console.log('User guessed: ' + anglophileGuess);
-  var seriousGuess = prompt('Question 5: Is Rob the sort of person to take himself seriously?');
-  // Not sure if serious.
-  console.log('User guessed: ' + seriousGuess);
+  console.log('User guessed: ' + sleepGuess + ', resolving as ' + sleepGuess[0].toUpperCase());
+  if (sleepGuess[0].toUpperCase() === 'Y'){
+    console.log('True');
+  } else {
+    console.log('False');
+  }
 
+  var motorcycleGuess = prompt('Question 2: Do you think Rob rides a motorcycle? \(Y\/N\)');
+  // I was *this* close!
+  console.log('User guessed: ' + motorcycleGuess + ', resolving as ' + motorcycleGuess[0].toUpperCase());
+  if (motorcycleGuess[0].toUpperCase() === 'Y'){
+    console.log('True');
+  } else {
+    console.log('False');
+  }
+
+  var montyPythonGuess = prompt('Question 3: Do you agree that three shall be the number thou shalt count, and the number of the counting shall be three? \(Y\/N\)');
+  // Five is right out.
+  console.log('User guessed: ' + montyPythonGuess + ', resolving as ' + montyPythonGuess[0].toUpperCase());
+  if (montyPythonGuess[0].toUpperCase() === 'Y'){
+    console.log('True');
+  } else {
+    console.log('False');
+  }
+  var anglophileGuess = prompt('Question 4: Do you think Rob also likes Red Dwarf and Doctor Who? \(Y\/N\)');
+  // Warning: possible Anglophile detected.
+  console.log('User guessed: ' + anglophileGuess + ', resolving as ' + anglophileGuess[0].toUpperCase());
+  if (anglophileGuess[0].toUpperCase() === 'Y'){
+    console.log('True');
+  } else {
+    console.log('False');
+  }
+
+  var seriousGuess = prompt('Question 5: Is Rob the sort of person to take himself seriously? \(Y\/N\)');
+  // Not sure if serious.
+  console.log('User guessed: ' + seriousGuess + ', resolving as ' + seriousGuess[0].toUpperCase());
+  if (seriousGuess[0].toUpperCase() === 'Y'){
+    console.log('True');
+  } else {
+    console.log('False');
+  }
 } else {
   // No quiz for you!
   console.log('User is NOT prepared.');
