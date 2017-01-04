751a59c31457667194c6d6a2228be691f2f6cc63
robert-hatfield <robert@lucidthought.xyz>
2017-01-03 23:25:37 -0800

    JavaScript reenabled. Changed questions to use defined string variables.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// set values for questions and the correct answers
+var question1 = 'Question 1: Do you think Rob has had more than six hours of sleep in the past two days? \(Y\/N\)';
+var question2 = 'Question 2: Do you think Rob rides a motorcycle? \(Y\/N\)';
+var question3 = 'Question 3: Do you agree that three shall be the number thou shalt count, and the number of the counting shall be three? \(Y\/N\)';
+var question4 = 'Question 4: Do you think Rob also likes Red Dwarf and Doctor Who? \(Y\/N\)';
+var question5 = 'Question 5: Is Rob the sort of person to take himself seriously? \(Y\/N\)';
+var correctAnswers = ['N','N','Y','Y','trick question'];
+
 // Check if user is ready.
 var answer1 = confirm('You\'re about to to make some guesses about Rob Hatfield. Are you ready?');
 
@@ -8,23 +16,23 @@ if (answer1 === true) {
   console.log('User has confirmed readiness.');
   alert('There are 5 Yes or No questions, so please answer accordingly.');
 
-  var sleepGuess = prompt('Question 1: Do you think Rob has had more than six hours of sleep in the past two days? \(Y\/N\)');
+  var sleepGuess = prompt(question1);
   // Because he sure hasn't.
   console.log('User guessed: ' + sleepGuess);
 
-  var motorcycleGuess = prompt('Question 2: Do you think Rob rides a motorcycle? \(Y\/N\)');
+  var motorcycleGuess = prompt(question2);
   // I was *this* close!
   console.log('User guessed: ' + motorcycleGuess);
 
-  var montyPythonGuess = prompt('Question 3: Do you agree that three shall be the number thou shalt count, and the number of the counting shall be three? \(Y\/N\)');
+  var montyPythonGuess = prompt(question3);
   // Five is right out.
   console.log('User guessed: ' + montyPythonGuess);
 
-  var anglophileGuess = prompt('Question 4: Do you think Rob also likes Red Dwarf and Doctor Who? \(Y\/N\)');
+  var anglophileGuess = prompt(question4);
   // Warning: possible Anglophile detected.
   console.log('User guessed: ' + anglophileGuess);
 
-  var seriousGuess = prompt('Question 5: Is Rob the sort of person to take himself seriously? \(Y\/N\)');
+  var seriousGuess = prompt(question5);
   // Not sure if serious.
   console.log('User guessed: ' + seriousGuess);
 
@@ -35,7 +43,6 @@ if (answer1 === true) {
     [anglophileGuess, false, false],
     [seriousGuess, false, false]];
 // set the correct answers to these questions
-  var correctAnswers = ['N','N','Y','Y','trick question'];
 
 // Checking all responses in an iterative loop is a lot cleaner
   for (var i = 0; i < 5 ; i++) {
