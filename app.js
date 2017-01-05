b328e7abe82ac14bf7b84f29dd2474f2af56bcc6
robert-hatfield <robert@lucidthought.xyz>
2017-01-04 20:28:37 -0800

    Yes/No quiz logic working

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 
 // create objects to contain question and response data
 var q1 = {
-  questionNth: '1st ',
-  question: 'Do you think I had more than six hours of sleep in the past two days while building this quiz? \(Y\/N\)',
+  question: 'Question 1: Do you think I had more than six hours of sleep in the past two days while building this quiz? \(Y\/N\)',
   correctAnswer: 'N',
   userResponse: '',
   nullCheck: false,
@@ -11,8 +10,7 @@ var q1 = {
   feedback: 'I didn\'t. But isn\'t that what weekends are for?'
 };
 var q2 = {
-  questionNth: '2nd ',
-  question: 'Do you think that I ride a motorcycle? \(Y\/N\)',
+  question: 'Question 2: Do you think that I ride a motorcycle? \(Y\/N\)',
   correctAnswer: 'N',
   userResponse: '',
   nullCheck: false,
@@ -20,8 +18,7 @@ var q2 = {
   feedback: 'unfortunately I don\'t. I came close, but just before going to a class to get my motorcycle endorsement, had to totally replace my car.'
 };
 var q3 = {
-  questionNth: '3rd ',
-  question: 'Do you agree that shalt thou count to three, no more, no less? \(Y\/N\)',
+  question: 'Question 3: Do you agree that shalt thou count to three, no more, no less? \(Y\/N\)',
   correctAnswer: 'Y',
   userResponse: '',
   nullCheck: false,
@@ -29,8 +26,7 @@ var q3 = {
   feedback: 'three shall be the number thou shalt count, and the number of the counting shall be three. Four shalt thou not count, neither count thou two, excepting that thou then proceed to three. Five is right out.'
 };
 var q4 = {
-  questionNth: '4th ',
-  question: 'Do you think Rob likes programmes such as Doctor Who, The IT Crowd, and Red Dwarf? \(Y\/N\)',
+  question: 'Question 4: Do you think Rob likes programmes such as Doctor Who, The IT Crowd, and Red Dwarf? \(Y\/N\)',
   correctAnswer: 'Y',
   userResponse: '',
   nullCheck: false,
@@ -38,8 +34,7 @@ var q4 = {
   feedback: '! In fact, I could be showing several early warning symptoms of becoming an Anglophile. Ask my doctor if cancelling BBC America is right for me.'
 };
 var q5 = {
-  questionNth: '5th ',
-  question: 'Is Rob the sort of person to take himself seriously? \(Y\/N\)',
+  question: 'Question 5: Is Rob the sort of person to take himself seriously? \(Y\/N\)',
   correctAnswer: 'trick',
   userResponse: '',
   nullCheck: false,
@@ -47,11 +42,14 @@ var q5 = {
   feedback: 'this is a trick question, which doesn\'t really have a yes or no answer. While I am guilty at times of taking things more seriously than strictly needed, I\'m also quite capable of choosing to make a complete fool of myself.'
 };
 
+// store all Y/N questions in an array
+var quizBoolean = [q1, q2, q3, q4, q5];
+
 // Check if user is ready.
 var readyCheck = confirm('You\'re about to to make some guesses about Robert. Are you ready?');
 
 // Log user response for readiness. No quiz if user clicks 'Cancel'.
-if (answer1 === true) {
+if (readyCheck === true) {
   console.log('User has confirmed readiness.');
   alert('There are 5 Yes or No questions, so please answer accordingly.');
 
@@ -64,35 +62,24 @@ if (answer1 === true) {
 
 // Checking all responses in an iterative loop is a lot cleaner
   for (var i = 0; i < 5 ; i++) {
-    var nowChecking = i + 1;
-    console.log(guessCheck[i]);
-    document.write('<p>The ' + guessCheck[i][3] + ' question was:<br />'
-    + question[i] + '</p>');
+    quizBoolean[i].userResponse = prompt(quizBoolean[i].question);
+    console.log(quizBoolean[i].userResponse);
     // check if answer was defined
-    if (guessCheck[i][0]) {
-      console.log('Question ' + nowChecking + ' is answered.' );
-      document.write('<p>Your ' + guessCheck[i][3] + ' guess was:<br />'
-      + guessCheck[i][0] + '</p>');
-      guessCheck[i][1] = true;
+    if (quizBoolean[i].userResponse) {
+      console.log('Question ' + (i + 1) + ' was answered.' );
+      quizBoolean[i].nullCheck = true;
       // then check if correct
-      console.log('First character of guess is ' + guessCheck[i][0][0]);
-      if (guessCheck[i][0][0].toUpperCase() == correctAnswers[i]) {
+      console.log('First character of guess is ' + quizBoolean[i].userResponse[0]);
+      if (quizBoolean[i].userResponse[0].toUpperCase() == quizBoolean[i].correctAnswer) {
         console.log('And is correct.');
-        document.write('<p>Nicely done!<br />'
-        + 'In fact, ' + guessCheck[i][4] + '</p>');
-        guessCheck[i][2] = true;
-        console.log(guessCheck[i]);
+        document.write('<p>Nicely done!<br />');
       } else {
         console.log('But is incorrect.');
-        document.write('<p>Sorry, that\'s wrong.<br />'
-        + 'Actually, ' + guessCheck[i][4] + '</p>');
-        guessCheck[i][2] = false;
-        console.log(guessCheck[i]);
+        document.write('<p>Sorry, that\'s wrong.<br />');
       }
     } else {
-      console.log('Question ' + nowChecking + ' was not answered.');
+      console.log('Question ' + (i + 1) + ' was not answered.');
       document.write('<p>But you didn\'t bother to answer it!</p>');
-      guessCheck[i][1] = false;
     }
   }
 
